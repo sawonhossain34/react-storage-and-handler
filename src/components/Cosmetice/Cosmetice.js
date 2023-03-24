7e36f26fbc8da6c960479bff9cfccf21ff6867f8
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-24 22:29:26 +0600

    first commit

---
+++ src/components/Cosmetice/Cosmetice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetice = () => {
+    return (
+        <div>
+            <h1>Wellcome to my Cosmetice store</h1>
+        </div>
+    );
+};
+
+export default Cosmetice;
