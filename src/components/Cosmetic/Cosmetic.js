28283fab90dead12a5cf4a49c2e8df18ab4da10e
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-25 11:53:09 +0600

    eventhandler add and with id in onClick

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     console.log(props);
     const {name,price,id} =props.cosmetic;
+
+    const addToCart = (id) => {
+        console.log('add item' , id)
+    }
+    // const purcheess = () => addToCart(id);
     return (
-        <div>
+        <div className='product'>
             <h1>Name : {name}</h1>
             <p>Price :{price}</p>
             <p>Id Name : {id}</p>
+            <button onClick={addToCart}>Add To Cart</button>
+            <button onClick={() => addToCart(id)}>Purchese</button>
         </div>
     );
 };
